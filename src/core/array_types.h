0a3a49c3e62ef26b037cd7c564fc106b0bb6b2e2
Robin Skånberg <robin.skanberg@liu.se>
2019-04-25 17:38:01 +0200

    Added own version of initializer list to array_types, this is to enable: DynamicArray<int> x = {1,2,4,5,6,7} type of initialization hopefully. Simplified and cleaned up some in DynamicArray

--- src/core/array_types.h
+++ src/core/array_types.h
@@ -6,6 +6,37 @@
 #include <string.h>
 #include <type_traits>
 
+template <class T>
+struct InitializerList {
+    using value_type = T;
+    using reference = const T&;
+    using const_reference = const T&;
+
+    using iterator = const T*;
+    using const_iterator = const T*;
+
+    constexpr InitializerList() noexcept = default;
+    constexpr InitializerList(const T* first_arg, const T* last_arg) noexcept : first(first_arg), last(last_arg) {}
+
+    constexpr const T* beg() const noexcept { return first; }
+    constexpr const T* begin() const noexcept { return first; }
+    constexpr const T* end() const noexcept { return last; }
+    constexpr int64 size() const noexcept { return (int64)(_Last - _First); }
+
+    const T* first = nullptr;
+    const T* last = nullptr;
+};
+
+template <class T>
+constexpr const T* begin(InitializerList<T> list) noexcept {
+    return list.begin();
+}
+
+template <class T>
+constexpr const T* end(InitializerList<T> list) noexcept {
+    return list.end();
+}
+
 /*
   This is an 'array-view' which exposes access to some data which is not owned by the array itself.
   Nothing will be allocated or freed by the constructors and destructors of this object.
@@ -14,7 +45,7 @@
 template <typename T>
 struct Array {
     using ElementType = T;
-    
+
     Array() = default;
     Array(T* _data, int64 _count) : ptr(_data), count(_count) {}
     Array(T* _data_beg, T* _data_end) : ptr(_data_beg), count(_data_end - _data_beg) {}
@@ -149,55 +180,44 @@ struct StaticArray : Array<T> {
 // @WARNING: THIS IS NOT A STRAIGHT FORWARD REPLACEMENT TO STD::VECTOR AS CONSTRUCTORS AND DESTRUCTORS ARE NEVER CALLED.
 template <typename T>
 struct DynamicArray : Array<T> {
-    static_assert(std::is_trivially_destructible<T>::value, "DynamicArray only supports trivially destructable data types");
+    static_assert(std::is_trivially_destructible<T>::value, "DynamicArray only supports trivially destructable 'POD' data types");
 
-    static constexpr int64 INIT_CAPACITY = 8;
-    DynamicArray() : m_capacity(INIT_CAPACITY) {
+    DynamicArray() noexcept : m_capacity(init_capacity()) {
         this->ptr = (T*)CALLOC(m_capacity, sizeof(T));
         this->count = 0;
     }
 
-    DynamicArray(int64 count) {
-        count < 1 ? count = 1 : count;
-        m_capacity = count;
-        this->ptr = (T*)CALLOC(m_capacity, sizeof(T));
-        this->count = m_capacity;
+    DynamicArray(int64 size) noexcept {
+		init(size);
     }
 
-    DynamicArray(int64 count, T value) : m_capacity(count > INIT_CAPACITY ? count : INIT_CAPACITY) {
-        this->ptr = (T*)MALLOC(m_capacity * sizeof(T));
-        this->count = count;
-        for (int i = 0; i < count; i++) {
-            this->ptr[i] = value;
+    DynamicArray(int64 size, const T& value) noexcept {
+        init(size);
+        if (size > 0) {
+            for (int64 i = 0; i < size; i++) {
+                this->ptr[i] = value;
+            }
         }
     }
 
     DynamicArray(const T* first, const T* last) noexcept {
-        this->m_capacity = last - first;
-        this->count = m_capacity;
-        if (this->count > 0) {
-            this->ptr = (T*)MALLOC(m_capacity * sizeof(T));
-            memcpy(this->ptr, first, this->count * sizeof(T));
-        }
+        const auto size = last - first;
+        init(size, first);
     }
 
-    DynamicArray(const Array<const T>& clone_source) : m_capacity(clone_source.count) {
-        this->count = m_capacity;
-        if (this->count > 0) {
-            this->ptr = (T*)MALLOC(m_capacity * sizeof(T));
-            memcpy(this->ptr, clone_source.ptr, this->count * sizeof(T));
-        }
+    DynamicArray(InitializerList<T> init_list) noexcept {
+        init(init_list.size(), init_list.begin());
     }
 
-    DynamicArray(const DynamicArray& other) : m_capacity(other.count) {
-        this->count = m_capacity;
-        if (this->count > 0) {
-            this->ptr = (T*)MALLOC(m_capacity * sizeof(T));
-            memcpy(this->ptr, other.ptr, this->count * sizeof(T));
-        }
+    DynamicArray(const Array<const T>& clone_source) noexcept {
+		init(clone_source.size(), clone_source.data());
     }
 
-    DynamicArray(DynamicArray&& other) {
+    DynamicArray(const DynamicArray& other) noexcept {
+		init(other.size(), other.data());
+    }
+
+    DynamicArray(DynamicArray&& other) noexcept {
         this->ptr = other.ptr;
         this->m_capacity = other.m_capacity;
         this->count = other.count;
@@ -209,11 +229,13 @@ struct DynamicArray : Array<T> {
     ~DynamicArray() {
         if (this->ptr) {
             FREE(this->ptr);
+            this->ptr = nullptr;
         }
         this->count = 0;
+        this->m_capacity = 0;
     }
 
-    DynamicArray& operator=(const Array<const T>& other) {
+    DynamicArray& operator=(const Array<const T>& other) noexcept {
         // Is this ok? It probably is since we're only comparing memory adresses...
         if (&other != (const Array<const T>*)this) {
             if (other.count > m_capacity) {
@@ -225,7 +247,7 @@ struct DynamicArray : Array<T> {
         return *this;
     }
 
-    DynamicArray& operator=(const DynamicArray& other) {
+    DynamicArray& operator=(const DynamicArray& other) noexcept {
         if (&other != this) {
             if (other.count > m_capacity) {
                 reserve(other.count);
@@ -236,7 +258,7 @@ struct DynamicArray : Array<T> {
         return *this;
     }
 
-    DynamicArray& operator=(DynamicArray&& other) {
+    DynamicArray& operator=(DynamicArray&& other) noexcept {
         // @NOTE: Is this check needed?
         if (&other != this) {
             if (this->ptr) {
@@ -252,45 +274,40 @@ struct DynamicArray : Array<T> {
         return *this;
     }
 
-    int64 capacity() const { return m_capacity; }
+    int64 capacity() const noexcept { return m_capacity; }
 
-    inline int64 _grow_capacity(int64 sz) const {
-        int64 new_capacity = m_capacity ? (m_capacity + m_capacity / 2) : INIT_CAPACITY;
-        return new_capacity > sz ? new_capacity : sz;
-    }
-
-    void append(Array<const T> arr) {
+    void append(Array<const T> arr) noexcept {
         if (this->count + arr.count >= m_capacity) {
-            reserve(_grow_capacity(this->count + arr.count));
+            reserve(grow_capacity(this->count + arr.count));
         }
         memcpy(this->end(), arr.ptr, arr.count * sizeof(T));
         this->count += arr.count;
     }
 
-    void append(Array<T> arr) {
+    void append(Array<T> arr) noexcept {
         if (this->count + arr.count >= m_capacity) {
-            reserve(_grow_capacity(this->count + arr.count));
+            reserve(grow_capacity(this->count + arr.count));
         }
         memcpy(this->end(), arr.ptr, arr.count * sizeof(T));
         this->count += arr.count;
     }
 
-    T& push_back(const T& item) {
+    T& push_back(const T& item) noexcept {
         if (this->count == m_capacity) {
-            reserve(_grow_capacity(this->count + 1));
+            reserve(grow_capacity(this->count + 1));
         }
         this->ptr[this->count] = item;
         this->count++;
         return this->back();
     }
 
-    T pop_back() {
+    T pop_back() noexcept {
         ASSERT(this->count > 0);
         this->count--;
         return this->ptr[this->count];
     }
 
-    void reserve(int64 new_capacity) {
+    void reserve(int64 new_capacity) noexcept {
         if (new_capacity < m_capacity) return;
         T* new_data = (T*)CALLOC(new_capacity, sizeof(T));
         if (this->ptr) {
@@ -302,31 +319,31 @@ struct DynamicArray : Array<T> {
     }
 
     // Resizes the array to a new size and zeros eventual new slots
-    void resize(int64 new_count) {
+    void resize(int64 new_count) noexcept {
         if (new_count == this->count) {
             return;
         } else if (new_count < this->count) {
             this->count = new_count;
         } else {
             if (m_capacity < new_count) {
-                reserve(_grow_capacity(new_count));
+                reserve(grow_capacity(new_count));
                 // memset(this->data + this->count, 0, (new_count - this->count) * sizeof(T));
             }
             this->count = new_count;
         }
     }
 
-    T* insert(T* it, const T& v) {
+    T* insert(T* it, const T& v) noexcept {
         ASSERT(this->beg() <= it && it <= this->end());
         const auto off = it - this->beg();
-        if (this->count == m_capacity) reserve(_grow_capacity(this->count + 1));
+        if (this->count == m_capacity) reserve(grow_capacity(this->count + 1));
         if (off < (int64)this->count) memmove(this->beg() + off + 1, this->beg() + off, ((size_t)this->count - (size_t)off) * sizeof(T));
         this->ptr[off] = v;
         this->count++;
         return this->beg() + off;
     }
 
-    void remove(T* it, int64 num_items = 1) {
+    void remove(T* it, int64 num_items = 1) noexcept {
         ASSERT(this->beg() <= it && it < this->end());
         ASSERT(it + num_items <= this->end());
         auto dst = it;
@@ -335,26 +352,49 @@ struct DynamicArray : Array<T> {
         this->count--;
     }
 
-    void swap_back_and_pop(T* it) {
+    void swap_back_and_pop(T* it) noexcept {
         ASSERT(this->beg() <= it && it < this->end());
-        *it = this->back();
+        if (it != &this->back()) *it = this->back();
         pop_back();
     }
 
-    void clear() { this->count = 0; }
+    void clear() noexcept { this->count = 0; }
 
 private:
-    int64 m_capacity;
+	inline void init(int64 size, T* src_data = nullptr) {
+        ASSERT(size >= 0);
+        this->m_capacity = init_capacity(size);
+        if (size > 0) {
+            this->ptr = (T*)MALLOC(this->m_capacity * sizeof(T));
+            ASSERT(this->ptr);
+            if (src_data) {
+                memcpy(this->ptr, src_data, size * sizeof(T));
+            }
+        }
+        this->count = size;
+	}
+
+    inline int64 init_capacity(int64 sz = 0) const noexcept {
+        constexpr int64 min_size = 8;
+        return sz > min_size ? sz : min_size;
+    }
+
+    inline int64 grow_capacity(int64 sz) const noexcept {
+        const int64 new_capacity = m_capacity != 0 ? (m_capacity + m_capacity / 2) : init_capacity(0);
+        return new_capacity > sz ? new_capacity : sz;
+    }
+
+    int64 m_capacity = 0;
 };
 
 template <typename T>
-Array<T> allocate_array(int64 num_elements) {
+Array<T> allocate_array(int64 num_elements) noexcept {
     if (num_elements == 0) return {};
     return {(T*)MALLOC(num_elements * sizeof(T)), num_elements};
 }
 
 template <typename T>
-void free_array(Array<T>* arr) {
+void free_array(Array<T>* arr) noexcept {
     ASSERT(arr);
     if (arr->ptr) {
         FREE(arr->ptr);
@@ -364,12 +404,12 @@ void free_array(Array<T>* arr) {
 }
 
 template <typename T>
-void zero_array(Array<T> arr) {
+void zero_array(Array<T> arr) noexcept {
     memset(arr.ptr, 0, arr.size_in_bytes());
 }
 
 template <typename T>
-void memset_array(Array<T> arr, const T& val) {
+void memset_array(Array<T> arr, const T& val) noexcept {
     ASSERT(arr);
     for (int64 i = 0; i < arr.count; i++) {
         *(arr.ptr + i) = val;
@@ -377,7 +417,7 @@ void memset_array(Array<T> arr, const T& val) {
 }
 
 template <typename T>
-void memset_array(Array<T> arr, const T& val, int64 offset, int64 length) {
+void memset_array(Array<T> arr, const T& val, int64 offset, int64 length) noexcept {
     ASSERT(arr);
     ASSERT(0 <= offset && offset < arr.count);
     ASSERT(0 < length && offset + length <= arr.count);
@@ -387,7 +427,7 @@ void memset_array(Array<T> arr, const T& val, int64 offset, int64 length) {
 }
 
 template <typename T>
-void memset_array(Array<T> arr, const T& val, Range<int32> range) {
+void memset_array(Array<T> arr, const T& val, Range<int32> range) noexcept {
     ASSERT(arr);
     ASSERT(0 <= range.beg && range.end <= arr.count);
     for (int32 i = range.beg; i < range.end; i++) {
@@ -396,7 +436,7 @@ void memset_array(Array<T> arr, const T& val, Range<int32> range) {
 }
 
 template <typename T>
-void memset_array(Array<T> arr, const T& val, Range<int64> range) {
+void memset_array(Array<T> arr, const T& val, Range<int64> range) noexcept {
     ASSERT(arr);
     ASSERT(0 <= range.beg && range.end <= arr.count);
     for (int64 i = range.beg; i < range.end; i++) {
@@ -406,14 +446,14 @@ void memset_array(Array<T> arr, const T& val, Range<int64> range) {
 
 // https://stackoverflow.com/questions/1493936/faster-approach-to-checking-for-an-all-zero-buffer-in-c
 template <typename T>
-bool is_array_zero(Array<const T> arr) {
+bool is_array_zero(Array<const T> arr) noexcept {
     const uint8* buf = (uint8*)arr.data();
     const auto size = arr.size_in_bytes();
     return buf[0] == 0 && !memcmp(buf, buf + 1, size - 1);
 }
 
 template <typename T>
-bool is_array_zero(Array<T> arr) {
+bool is_array_zero(Array<T> arr) noexcept {
     const uint8* buf = (uint8*)arr.data();
     const auto size = arr.size_in_bytes();
     return buf[0] == 0 && !memcmp(buf, buf + 1, size - 1);
