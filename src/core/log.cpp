4f21acc64e18bf2eff6896a38531a435c1644041
Robin Skånberg <robin.skanberg@liu.se>
2019-10-15 13:42:31 +0200

    Added time stamp to log

--- src/core/log.cpp
+++ src/core/log.cpp
@@ -1,6 +1,7 @@
 #include <core/log.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <time.h>
 
 namespace logging {
 
@@ -10,19 +11,25 @@ struct LoggingBackend {
 };
 
 static DynamicArray<LoggingBackend> entries;
-static constexpr int32 BUF_SIZE = KILOBYTES(64); // @NOTE (Robin): Ought to be enough for everyone, right?
+static constexpr int32 BUF_SIZE = KILOBYTES(64); // @NOTE (Robin): Ought to be enough for everyone, right?!
 static char buf[BUF_SIZE];
 
 void initialize() {}
-
 void shutdown() {}
 
 void register_backend(LoggingFunc func, void* usr_data) { entries.push_back({func, usr_data}); }
 
 void record(Severity severity, const char* format, ...) {
+
+    time_t now = time(0);
+    struct tm tstruct;
+    tstruct = *localtime(&now);
+
     va_list ap;
     va_start(ap, format);
-    int count = vsnprintf(buf, BUF_SIZE, format, ap);
+    int count = 0;
+    count += strftime(buf, BUF_SIZE, "%T: ", &tstruct);
+    count += vsnprintf(buf + count, BUF_SIZE, format, ap);
     va_end(ap);
     CStringView str(buf, count);
 
