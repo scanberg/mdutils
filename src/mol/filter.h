6886e94683e5aa571b06cfc717f5b8e582745be9
Robin Skånberg <scanberg@gmail.com>
2019-11-30 21:32:42 +0100

    cleanup of unused function

--- src/mol/filter.h
+++ src/mol/filter.h
@@ -17,20 +17,4 @@ void shutdown();
 bool compute_filter_mask(Bitfield mask, CStringView filter, const MoleculeStructure& molecule, ArrayView<const StoredSelection> stored_selectons = {});
 bool filter_uses_selection(CStringView filter, ArrayView<const StoredSelection> stored_selectons);
 
-template <typename T>
-void extract_filtered_data(DynamicArray<T>* dst_data, ArrayView<const T> src_data, ArrayView<const bool> mask) {
-    ASSERT(dst_data);
-    dst_data->clear();
-    for (int32 i = 0; i < mask.count; i++) {
-        if (mask[i]) dst_data->push_back(src_data[i]);
-    }
-}
-
-template <typename T>
-DynamicArray<T> extract_filtered_data(ArrayView<const T> data, ArrayView<const bool> mask) {
-    DynamicArray<T> result{};
-    extract_filtered_data(&result, data, mask);
-    return result;
-}
-
 }  // namespace filter
