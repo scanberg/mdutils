df1e6a788a54e42262615d7c17d671f5ee39d167
Robin Skånberg <scanberg@gmail.com>
2019-11-30 21:31:39 +0100

    Added fmemopen functionality for win32, this is experimental and may not stick around...

--- src/core/file.h
+++ src/core/file.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "string_types.h"
-
 #include <stdio.h>
+
+// Wrapper for fopen to support utf-8 character encoding.
 
 FILE* fopen_utf8(const char* filename, const char* mode);
 FILE* fopen(CStringView filename, CStringView mode);
+
+FILE* fmemopen(void* buf, size_t len, const char* mode);
