28a270d82049008e2c78a35bd54a8e25b69efd46
Robin Skånberg <scanberg@gmail.com>
2020-03-16 07:16:50 +0100

    bug fix to aminoacid comparison function

--- src/mol/aminoacid_utils.h
+++ src/mol/aminoacid_utils.h
@@ -13,7 +13,7 @@ constexpr AminoAcid get_amino_acid_from_string(CStringView cstr) {
     };
 
     if (cstr.count != 3) return AminoAcid::Unknown;
-    const char seq[3] = {to_upper(cstr[0]), to_upper(cstr[1]), to_upper(cstr[2])};
+    const char seq[4] = {to_upper(cstr[0]), to_upper(cstr[1]), to_upper(cstr[2]), '\0'};
     CStringView sv = seq;
 
     for (unsigned int i = 0; i < aminoacid::num_amino_acids; i++) {
