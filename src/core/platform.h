caa9e83a297b0e41035db9822faeb88df61d9594
Robin Skånberg <scanberg@gmail.com>
2020-06-06 00:30:33 +0200

    Fixed platform defines

--- src/core/platform.h
+++ src/core/platform.h
@@ -4,26 +4,40 @@
 #define PLATFORM_LINUX   0
 #define PLATFORM_OSX     0
 
-#if defined _WIN32 || defined _WIN64
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif  // NOMINMAX
+#define COMPILER_GCC     0
+#define COMPILER_CLANG   0
+#define COMPILER_MSVC    0
+#define COMPILER_UNKNOWN 0
 
-#undef  PLATFORM_WINDOWS
-#define PLATFORM_WINDOWS 1
-
+#if defined(_WIN32) || defined(_WIN64)
+    #undef  PLATFORM_WINDOWS
+    #define PLATFORM_WINDOWS 1
+    #ifndef NOMINMAX
+        #define NOMINMAX
+    #endif
 #elif __APPLE__
-#include "TargetConditionals.h"
-#ifdef TARGET_OS_MAC
-#undef  PLATFORM_OSX
-#define PLATFORM_OSX 1
+    #include "TargetConditionals.h"
+    #ifdef  TARGET_OS_MAC
+        #undef  PLATFORM_OSX
+        #define PLATFORM_OSX 1
+    #endif
+#elif defined __linux__
+    #undef  PLATFORM_LINUX
+    #define PLATFORM_LINUX 1
+#else
+    #error "Platform Unsuported"
 #endif
 
-#elif defined __linux__
-#undef  PLATFORM_LINUX
-#define PLATFORM_LINUX 1
+#if defined(_MSC_VER)
+#undef  COMPILER_MSVC
+#define COMPILER_MSVC 1
+#elif defined(__GNUC__) && !defined(__clang__)
+    #undef  COMPILER_GCC
+    #define COMPILER_GCC 1
+#elif defined(__clang__)
+    #undef  COMPILER_CLANG
+    #define COMPILER_CLANG 1
 #else
-
-#error "Platform Unsuported"
-
+    #undef  COMPILER_UNKNOWN
+    #define COMPILER_UNKNOWN 1
 #endif
